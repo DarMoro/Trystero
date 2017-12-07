1fd98329064c19be33c25a408ed770fcc65e9c2d
Darren <me@email.com>
2017-12-07 13:12:20 +0000

    This is the version as of 07_12_2017

---
+++ PageOne/js/app.js
@@ -0,0 +1,6 @@
+$('button').click(function() {
+  $(this).toggleClass('expanded').siblings('div').slideToggle();
+});
+
+
+
---
+++ js/app.js
@@ -0,0 +1,49 @@
+$('button').click(function() {
+  $(this).toggleClass('expanded').siblings('div').slideToggle();
+});
+
+
+
+$(document).click(function() {
+	$(".para1").toggle("slide");
+});
+
+
+// $(".para2").animate({width:'toggle'}, 350);
+
+
+
+$('#wrapper1').scroll(function(){
+    $('.para2').animate({'marginLeft':0}, 2000);
+
+
+
+
+
+
+    var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
